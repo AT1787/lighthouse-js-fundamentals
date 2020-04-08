df7d894ed4743056e59942fa8c8165cd3b6e5f20
Andrew Trac <andrewtrac87@hotmail.com>
2020-04-08 17:17:22 +0000

    revision to whichSchool.js file

---
+++ whichSchool.js
@@ -0,0 +1,13 @@
+const whichSchool  = function (age) {
+  if (age < 13) {
+    console.log('Elementary School');
+  } 
+  else if (age >= 13 && age <= 18) {
+    console.log('Secondary School');
+  }
+  else {
+    console.log('Lighthouse Labs');
+  }
+};
+
+whichSchool(50)
